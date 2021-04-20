399b83960630c5e42ce0a7cfed4a298f84ea5371
jbg <joaobenine@gmail.com>
2021-04-20 20:40:14 -0300

    add contentin header + import for view

---
+++ src/pages/components/Header/index.tsx
@@ -0,0 +1,14 @@
+export function Header(){
+    return(
+        <header>
+            <img src="/logo.svg" alt="Podcastr"/>
+
+            <p> O melhor para você ouvir, sempre.</p>
+
+            <span>Qui, 8 de Abril</span>
+        </header>
+
+    )
+
+
+}
