7fe05d3c171a6aa2e00766fd904bc91b9c9949a2
jbg <joaobenine@gmail.com>
2021-04-20 20:14:08 -0300

    ajustando estrutura

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,21 +1,21 @@
 import Document, {Html, Head, Main, NextScript } from 'next/document';
 
 
-export default class myDocument extends Document{
+export default class MyDocument extends Document{
     render(){
-        return(
-            <Html>
-                <Head>
-                    <link rel="preconnect" href="https://fonts.gstatic.com" />
-                    <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet" />
-                    <body>
-                        <Main />
-                            <NextScript />
-                    </body>
-                </Head>
-            </Html>
+    return(
+        <Html>
+            <Head>
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
 
-        )
+        );
     }
 }
 
